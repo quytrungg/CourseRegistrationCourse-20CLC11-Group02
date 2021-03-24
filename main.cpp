ba5f6191335fc7b8d6b35dfea0ffff44c34d013c
St0nks ✧ treblA <73238186+quytrungg@users.noreply.github.com>
2021-03-24 21:40:10 +0700

    test file hello world

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Mai Quý Trung on 24/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
