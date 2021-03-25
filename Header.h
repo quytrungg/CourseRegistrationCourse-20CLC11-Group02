d9ac30424976670ff9eff7c36789139d78edbf22
St0nks ✧ treblA <73238186+quytrungg@users.noreply.github.com>
2021-03-25 21:23:32 +0700

    struct sơ sơ :<

--- Header.h
+++ Header.h
@@ -1,1 +1,87 @@
 #ifndef HEADER_H
+#define HEADER_H
+
+#include <iostreams
+#include <fstream>
+#include <string>
+
+struct Student
+{
+	int no;
+	int id;
+	std::string fname;
+	std::string lname;
+	bool gender;
+	int dob;
+	int soid;
+	//class
+	//course
+	//score
+};
+
+struct Class
+{
+	std::string name;
+	Student stu;
+};
+
+struct Semester
+{
+	int term;
+	int year;
+	//start;
+	//end;
+	//course
+};
+
+struct Date
+{
+	int day;
+	int month;
+	int year;
+};
+
+struct Time
+{
+	int hour;
+	int min;
+	int sec;
+};
+
+struct DateTime
+{
+	Date d;
+	Time t;
+};
+
+struct Course
+{
+	int id;
+	std::string name;
+	std::string teacher;
+	int numofcre;
+	int maxstu;
+	//session
+	//class
+	//score
+};
+
+struct Account
+{
+	std::string user;
+	std::string pass;
+};
+
+struct Score
+{
+	//student's data
+	double total;
+	double final;
+	double midterm;
+	double other;
+	double gpa;
+	double ovrgpa;
+};
+
+#endif // !HEADER_H
+
--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
-//
-//  main.cpp
-//  test
-//
-//  Created by Mai Quý Trung on 24/03/2021.
-//
-
-#include <iostream>
+#include "Header.h"
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello World" << std::endl;
+    
     return 0;
 }
