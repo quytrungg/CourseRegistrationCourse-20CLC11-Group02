8e3a8ffd4ec3a3dfa3b8f641c71fd2f1de16343f
PHÚC TRẦN HỒNG MINH <20127285@student.hcmus.edu.vn>
2021-04-12 19:49:00 +0700

    Fixed xuất ra filed được rồi

--- Đồ án/Đồ án/Function.cpp
+++ Đồ án/Đồ án/Function.cpp
@@ -25,25 +25,31 @@ void ChangeToVietNamese()
 
 void OutputData(in4_student*&stu)
 {
-    ChangeToVietNamese();
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stdout), _O_U16TEXT);
     std::wfstream fout;
-    fout.open("dddd.csv", std::wfstream::out);
-    fout << wchar_t(239);
-    fout << wchar_t(187);
-    fout << wchar_t(191);
+    fout.open("output.csv", std::wfstream::out);
     fout.imbue(std::locale(fout.getloc(), new std::codecvt_utf8_utf16<wchar_t>));
-    
-    //in4_student* temp = stu;
+    fout << wchar_t(0xfeff);
+    in4_student* temp = stu;
     stu = stu->pPrev;
-    //delete temp;
-    in4_student* cur = stu;
-    while (cur != nullptr)
+    delete temp;
+    in4_student* cur = stu->pPrev;
+    if (cur == nullptr)
+    {
+        std::wcout << "Null";
+        return;
+    }
+    do
     {
         fout << cur->id << L',' << cur->fname << L',' << cur->lname << L',' << cur->gender << L',' << cur->dob << L',' << cur->soid << "\n";
         cur = cur->pPrev;
-    }
+    }while (cur != nullptr);
+
 
     fout.close();
+    _setmode(_fileno(stdin),  _O_TEXT);
+    _setmode(_fileno(stdout), _O_TEXT);
 }
 
 in4_student* Inputdata(in4_student*& t, std::wfstream &fin)
@@ -56,7 +62,6 @@ in4_student* Inputdata(in4_student*& t, std::wfstream &fin)
     {
         ChangeToVietNamese();
         std::getline(fin, temp);
-        std::wcout << temp << "\n";
         if (temp.length() != 0) add_student(t,ChangeToData(temp));
     }
     return t;
@@ -129,23 +134,39 @@ in4_student ChangeToData(std::wstring line)
 void PrintStu(in4_student*& data)
 {
     ChangeToVietNamese();
+
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    std::wfstream fout;
+    fout.open("output.csv", std::wfstream::out);
+    fout.imbue(std::locale(fout.getloc(), new std::codecvt_utf8_utf16<wchar_t>));
+    fout << wchar_t(0xfeff);
+
     in4_student* temp = data;
     data = data->pPrev;
     delete temp;
     in4_student* cur = data;
     while (cur != nullptr)
     {
+
         std::wcout << cur->id;
         std::wcout << " "  << cur->fname;
         std::wcout << std::setw(10) << cur->lname;
         std::wcout << std::setw(10) << cur->gender;
         std::wcout << "  " << cur->dob;
         std::wcout << std::setw(8) << cur->soid << "\n";
+
+        fout << cur->id << L',' << cur->fname << L',' << cur->lname << L',' << cur->gender << L',' << cur->dob << L',' << cur->soid << "\n";
+
         cur = cur->pPrev;
     }
+
+    fout.close();
+    _setmode(_fileno(stdin), _O_TEXT);
+    _setmode(_fileno(stdout), _O_TEXT);
 }
 
-void DeallocateData(in4_student* infor)
+void DeallocateData(in4_student*& infor)
 {
     in4_student* temp = infor;
     while (infor != nullptr)
