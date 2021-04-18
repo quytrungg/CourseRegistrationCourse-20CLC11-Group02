dd88d5d8a87ceff816bc9843c2fdebe90a15cb88
quytrungg <73238186+quytrungg@users.noreply.github.com>
2021-04-18 16:21:54 +0700

    commit tiếp :>

--- course(QT)/csv/Funciton2.h
+++ course(QT)/csv/Funciton2.h
@@ -21,6 +21,28 @@
 #include <codecvt>
 #include <sstream>
 
+#define On_Black  "\033[40m"//tô nền màu đen
+#define On_White  "\033[0;107m" //tô nền màu trắng
+#define On_Yellow  "\033[0;103m" //to nen mau vang
+#define RESET   "\033[0m" // đưa về nguyên bản
+#define BLACK   "\033[1m\033[30m" //xuất chữ màu đen nhạt
+#define ALLBLACK   "\033[30m" //xuất chữ màu đen
+#define RED     "\033[31m" //xuất chữ màu đỏ
+#define GREEN   "\033[32m" //xuất chữ màu lục
+#define YELLOW  "\033[33m" //xuất chữ màu vàng
+#define BLUE    "\033[34m" //xuất chữ màu lam
+#define MAGENTA "\033[35m"//xuất chữ màu tím
+#define CYAN    "\033[36m" //xuất chữ màu xanh dương nhạt
+#define WHITE   "\033[37m" //xuất chữ màu vàng trắng
+#define _SIZE 14    // kích cỡ bàn cờ
+#define _LONGSIZE 120 // chiều dài console
+#define KEY_UP 72 //phím di chuyển lên
+#define KEY_DOWN 80 //phím di chuyển xuống
+#define KEY_LEFT 75 //phím di chuyển trái
+#define KEY_RIGHT 77 //phím di chuyển phải
+#define KEY_ENTER 13//phim enter
+#define KEY_ESC 27// phim esc
+
 const std::string currentDateTime();
 char* StringToCharPointer(std::string text);
 bool comparei(std::wstring stringA , std::wstring stringB);
